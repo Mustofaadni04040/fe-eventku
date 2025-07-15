5f40e98521890b08d88f77fdc9d671042aba8df9
mustofa adny <adniadni04@gmail.com>
2025-07-15 12:53:24 +0700

    fix: toaster context

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { ToasterContext } from "@/context/ToasterContext";
+import { useContext } from "react";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const { setToaster } = useContext(ToasterContext);
+  return (
+    <div>
+      Hello World
+      <button
+        className="border border-slate-400 py-1 px-2 rounded-md"
+        onClick={() =>
+          setToaster({
+            variant: "success",
+            message: "This is a success message!",
+          })
+        }
+      >
+        Show Toaster
+      </button>
+    </div>
+  );
 }
--- components/ui/Toaster/index.tsx
+++ components/ui/Toaster/index.tsx
@@ -2,24 +2,22 @@
 
 import { ToasterContext } from "@/context/ToasterContext";
 import { ToasterType } from "@/types/toaster.type";
+import { X } from "lucide-react";
 import React, { useContext, useEffect, useRef, useState } from "react";
 
 const ToasterVariant: any = {
   success: {
     title: "Success",
-    icon: "bx-check-circle",
     color: "#a3d9a5",
     barColor: "#3f9242",
   },
   danger: {
     title: "Error",
-    icon: "bx-x-circle",
     color: "#f39b9a",
     barColor: "#bb2525",
   },
   warning: {
     title: "Warning",
-    icon: "bx-error",
     color: "#f8e3a2",
     barColor: "#e9d949",
   },
@@ -56,19 +54,17 @@ export default function Toaster() {
         `toaster--${toaster.variant}`,
       ]}`}
     >
-      <div className="flex items-center justify-center gap-4">
-        <div className="text-3xl">
-          <i className={`bx ${ToasterVariant[`${toaster.variant}`].icon}`} />
-        </div>
-        <div className="border-l-[1px] border-solid border-slate-400 pl-5 min-w-52">
+      <div>
+        <div className="border-solid border-slate-400 min-w-52">
           <p className="text-lg font-bold mb-1">
             {ToasterVariant[`${toaster.variant}`].title}
           </p>
           <p className="text-sm mb-1">{toaster.message}</p>
         </div>
-        <i
-          className={`bx bx-x absolute top-2 right-2 cursor-pointer text-2xl`}
+        <X
           onClick={() => setToaster({})}
+          size={15}
+          className="absolute top-2 right-2 cursor-pointer text-2xl"
         />
       </div>
       <div
