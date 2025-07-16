3a91f06834dc30c450aedb4305a48995711059f8
mustofa adny <adniadni04@gmail.com>
2025-07-16 13:21:35 +0700

    feat: add fetchData & formatDate helper

---
+++ utils/fetchData.ts
@@ -0,0 +1,18 @@
+import instance from "@/configs/instance";
+
+export function getData(url: string, params: any, token: string) {
+  return instance.get(`${url}`, {
+    params,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+export async function postData(url: string, payload: any, token: string) {
+  return await instance.post(`${url}`, payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
